f0c4d7a996d8e1d9af801eb32f2b03f4a6a4f2cd
senicko <sebastianflajszer@gmail.com>
2022-12-03 23:28:35 +0100

    Lox language expression parser written in java.
    The basic scanner & parser implemented following Crafting Interpreters book, with additional ternary operator parser.

---
+++ src/main/java/com/github/senicko/lox/TokenType.java
@@ -0,0 +1,26 @@
+package com.github.senicko.lox;
+
+public enum TokenType {
+    // Single-character tokens
+    LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+    QUESTION_MARK, COLON,
+
+    // One or two character tokens
+    BANG, BANG_EQUAL,
+    EQUAL, EQUAL_EQUAL,
+    GREATER, GREATER_EQUAL,
+    LESS, LESS_EQUAL,
+
+    // Literals
+    IDENTIFIER, STRING, NUMBER,
+
+    // Keywords
+    AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+    PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    EOF
+}
+
+
+
